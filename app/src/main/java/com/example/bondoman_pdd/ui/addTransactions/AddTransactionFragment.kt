d7f2b96bda6f8af6b6117a63cb0fd12664d40cf3
jason <13521008@std.stei.itb.ac.id>
2024-03-21 04:11:04 +0700

    fix: spinner dropdown

--- app/src/main/java/com/example/bondoman_pdd/ui/addTransactions/AddTransactionFragment.kt
+++ app/src/main/java/com/example/bondoman_pdd/ui/addTransactions/AddTransactionFragment.kt
@@ -1,13 +1,17 @@
 package com.example.bondoman_pdd.ui.addTransactions
 
-import android.content.Context
 import android.content.IntentFilter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.bondoman_pdd.R
 import com.example.bondoman_pdd.databinding.FragmentAddTransactionBinding
 import com.example.bondoman_pdd.ui.settings.ACTION_RANDOMIZE_TRANSACTION
 
@@ -15,6 +19,7 @@ class AddTransactionFragment : Fragment() {
 
     private lateinit var binding: FragmentAddTransactionBinding
     private lateinit var broadcastReceiver: MyBroadcastReceiver
+    private var spinner: Spinner? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -42,10 +47,49 @@ class AddTransactionFragment : Fragment() {
         requireContext().registerReceiver(broadcastReceiver, filter, receiverFlags)
     }
 
+    override fun onResume() {
+        super.onResume()
+
+        if (spinner == null) {
+            initSpinner()
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
 
         // Unregister the BroadcastReceiver
         requireContext().unregisterReceiver(broadcastReceiver)
+    }
+
+    private fun initSpinner() {
+        // Access the items of the list
+        val languages = resources.getStringArray(R.array.kategori_array)
+
+        // Access the spinner
+        spinner = binding.kategori
+        spinner?.let {
+            val adapter = ArrayAdapter(requireContext(),
+                android.R.layout.simple_spinner_item, languages)
+            it.adapter = adapter
+
+            it.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View,
+                    position: Int,
+                    id: Long
+                ) {
+                    Toast.makeText(
+                        requireContext(),"Selected Item: "+ languages[position],
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // Handle no selection
+                }
+            }
+        }
     }
 }
