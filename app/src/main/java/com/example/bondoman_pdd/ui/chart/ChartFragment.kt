6cd9c62f006a9f83858a5eb01b6d1c6d730c06bf
M. Malik I. Baharsyah <13521029@std.stei.itb.ac.id>
2024-04-03 02:41:01 +0000

    Revert "Merge branch 'transaction_after_login' of..."
    This reverts commit ddef48e89531ad9d8969ca7587d714303194cd35

--- app/src/main/java/com/example/bondoman_pdd/ui/chart/ChartFragment.kt
+++ app/src/main/java/com/example/bondoman_pdd/ui/chart/ChartFragment.kt
@@ -4,16 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.bondoman_pdd.R
-import com.example.bondoman_pdd.data.transactions.setup
 import com.example.bondoman_pdd.databinding.FragmentChartBinding
-import com.github.mikephil.charting.charts.PieChart
-import com.github.mikephil.charting.data.PieData
-import com.github.mikephil.charting.data.PieDataSet
-import com.github.mikephil.charting.data.PieEntry
-import com.github.mikephil.charting.utils.ColorTemplate
 
 class ChartFragment : Fragment() {
 
@@ -39,32 +33,6 @@ class ChartFragment : Fragment() {
 //            textView.text = it
 //        }
         return root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        // Membuat chart ditaru di id 'pieChart'
-        val pieChar : PieChart = view.findViewById(R.id.pieChart)
-        val entries : ArrayList<PieEntry> = ArrayList()
-
-        val setupData = setup.DatabaseHelper(requireContext())
-        val totalPemasukan : Float = setupData.getTotalPemasukan(13521028)
-        val totalPengeluaran : Float = setupData.getTotalPengeluaran(13521028)
-        println("Pemasukan : $totalPemasukan")
-        println("Pengeluaran : $totalPengeluaran")
-        entries.add(PieEntry(totalPemasukan, "Pemasukan"))
-        entries.add(PieEntry(totalPengeluaran, "Pengeluaran"))
-
-        // Membuat data set
-        val dataSet = PieDataSet(entries, "Election Results")
-        dataSet.setColors(*ColorTemplate.COLORFUL_COLORS)
-
-        // Membuat data
-        val data = PieData(dataSet)
-        pieChar.data = data
-        pieChar.invalidate()
-        println("ChartFragment.onCreateView")
     }
 
     override fun onDestroyView() {
