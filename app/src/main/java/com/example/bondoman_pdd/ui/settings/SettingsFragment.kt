8c8d586ae66cb75daacab7fc32ff7ad02b759f47
Malik Baharsyah <13521029@mahasiswa.itb.ac.id>
2024-04-04 23:09:17 +0700

    feat: fix

--- app/src/main/java/com/example/bondoman_pdd/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/bondoman_pdd/ui/settings/SettingsFragment.kt
@@ -9,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -52,69 +53,62 @@ class SettingsFragment : Fragment() {
             // delete token
             SecureStorage.deleteToken(requireContext())
             SecureStorage.deleteEmail(requireContext())
-            Log.d("SettingsFragment","${SecureStorage.getToken(requireContext())} token removed")
+            Log.d("SettingsFragment", "${SecureStorage.getToken(requireContext())} token removed")
 
             // Create an Intent to navigate back to LoginActivity
             val intent = Intent(requireActivity(), LoginActivity::class.java)
+        }
+            sendButton.setOnClickListener {
+                // Create an Intent to send an email
+                val recipient = "13521008@std.stei.itb.ac.id"
+                val subject = "Hello World"
+                val message = "Semoga harimu indah bro"
 
-        sendButton.setOnClickListener {
-            // Create an Intent to send an email
-            val recipient = "13521008@std.stei.itb.ac.id"
-            val subject = "Hello World"
-            val message = "Semoga harimu indah bro"
+                // Ni nanti ganti sama file xlsxnya kalo udah bisa
+                val fileUri = Uri.parse("content://path/to/your/file.xlsx")
 
-            // Ni nanti ganti sama file xlsxnya kalo udah bisa
-            val fileUri = Uri.parse("content://path/to/your/file.xlsx")
+                sendEmail(recipient, subject, message, fileUri)
+            }
 
-            sendEmail(recipient, subject, message, fileUri)
+            randomizeTransactionButton.setOnClickListener {
+                println("Randomize ditekan")
+                randomizeTransaction()
+                println("Randomize Transaction jalan")
+                Toast.makeText(requireContext(), "Randomize Transaction", Toast.LENGTH_SHORT).show()
+                findNavController().navigate(R.id.action_settingsFragment_to_addTransactionFragment)
+            }
         }
 
-        randomizeTransactionButton.setOnClickListener {
-            println("Randomize ditekan")
-            randomizeTransaction()
-            println("Randomize Transaction jalan")
-            Toast.makeText(requireContext(), "Randomize Transaction", Toast.LENGTH_SHORT).show()
-            findNavController().navigate(R.id.action_settingsFragment_to_addTransactionFragment)
+        override fun onDestroyView() {
+            super.onDestroyView()
+            _binding = null
+        }
+
+        private fun sendEmail(recipient: String, subject: String, message: String, fileUri: Uri) {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "application/octet-stream"
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(recipient))
+            intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+
+            // Set package name to Gmail
+            intent.setPackage("com.google.android.gm")
+
+            // Add the file as an attachment
+            intent.putExtra(Intent.EXTRA_STREAM, fileUri)
+
+            try {
+                startActivity(intent)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        private fun randomizeTransaction() {
+            val intent = Intent().also { intent ->
+                intent.setAction(ACTION_RANDOMIZE_TRANSACTION)
+            }
+            requireContext().sendBroadcast(intent)
+            println("Broadcast sent successfully")
         }
     }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
-    private fun logout() {
-        val intent = Intent(requireActivity(), LoginActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        startActivity(intent)
-        requireActivity().finish()
-    }
-
-    private fun sendEmail(recipient: String, subject: String, message: String, fileUri: Uri) {
-        val intent = Intent(Intent.ACTION_SEND)
-        intent.type = "application/octet-stream"
-        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(recipient))
-        intent.putExtra(Intent.EXTRA_SUBJECT, subject)
-        intent.putExtra(Intent.EXTRA_TEXT, message)
-
-        // Set package name to Gmail
-        intent.setPackage("com.google.android.gm")
-
-        // Add the file as an attachment
-        intent.putExtra(Intent.EXTRA_STREAM, fileUri)
-
-        try {
-            startActivity(intent)
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-
-    private fun randomizeTransaction() {
-        val intent = Intent().also { intent ->
-            intent.setAction(ACTION_RANDOMIZE_TRANSACTION)
-        }
-        requireContext().sendBroadcast(intent)
-        println("Broadcast sent successfully")
-    }
-}
